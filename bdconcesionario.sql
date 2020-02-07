2cfaef7445317ab8d603abee70b0e7b0d7712780
carlossan1978 <50367557+carlossan1978@users.noreply.github.com>
2020-02-06 21:34:56 -0500

    Script BD
    Script de la base de datos.

---
+++ bdconcesionario.sql
@@ -0,0 +1,78 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3308
+-- Generation Time: Feb 07, 2020 at 02:33 AM
+-- Server version: 5.7.23
+-- PHP Version: 7.2.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `bdconcesionario`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cliente`
+--
+
+DROP TABLE IF EXISTS `cliente`;
+CREATE TABLE IF NOT EXISTS `cliente` (
+  `IdCliente` int(11) NOT NULL AUTO_INCREMENT,
+  `Nombre` varchar(200) NOT NULL,
+  `Cedula` varchar(30) NOT NULL,
+  `Ciudad` varchar(30) DEFAULT NULL,
+  `Direccion` varchar(100) DEFAULT NULL,
+  `Telefono` varchar(30) DEFAULT NULL,
+  `Celular` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`IdCliente`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `cliente`
+--
+
+INSERT INTO `cliente` (`IdCliente`, `Nombre`, `Cedula`, `Ciudad`, `Direccion`, `Telefono`, `Celular`) VALUES
+(1, 'Juan Perez', '71776428', 'Medellin', 'Cra 80 No 20-410', '24655', '319785459');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `vehiculo`
+--
+
+DROP TABLE IF EXISTS `vehiculo`;
+CREATE TABLE IF NOT EXISTS `vehiculo` (
+  `IdVehiculo` int(11) NOT NULL AUTO_INCREMENT,
+  `Modelo` varchar(50) NOT NULL,
+  `MARCA` varchar(50) NOT NULL,
+  `Precio` decimal(10,0) NOT NULL,
+  `Descripcion` varchar(600) NOT NULL,
+  PRIMARY KEY (`IdVehiculo`)
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `vehiculo`
+--
+
+INSERT INTO `vehiculo` (`IdVehiculo`, `Modelo`, `MARCA`, `Precio`, `Descripcion`) VALUES
+(1, 'Sandero', 'Renault', '14006', 'Nuevo'),
+(4, 'Vitara', 'Chevrolet', '66666', 'Azul'),
+(2, 'March', 'Nissan', '5564', 'Lujo');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
